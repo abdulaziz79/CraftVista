43aad9ff6a3767c37c27ac93c513f4e67d1ebf2a
abdulaziz <aboudecharkawi@gmail.com>
2024-03-01 12:12:43 +0200

    responsive

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,8 +7,6 @@ import img2 from "../../assets/images/background.jpg"
 import img3 from "../../assets/images/backgroundd.jpg"
 import { Link } from 'react-router-dom'
 
-
-
 const textVariants ={
   initial:{
     x: -500,
@@ -63,8 +61,8 @@ const Hero = () => {
         <motion.div className={Styles.container} variants={textVariants} >
           <div className={Styles.left}>
           <div className={Styles.heroBackgrd}></div>
-          <motion.p className={Styles.p} variants={textVariants} initial="initial" animate="animate">Discover skilled<br /> <span style={{color:"orange", fontWeight:"700"}}>  professionals </span> </motion.p>
-           <motion.p variants={textVariants} className={Styles.text} initial="initial" animate="animate"> <span> near you </span> </motion.p>
+          <motion.p className={Styles.p} variants={textVariants} initial="initial" animate="animate">Discover skilled<br /> <span style={{color:"orange", fontWeight:"700"}}>  professionals </span> <br />near you</motion.p>
+           {/* <motion.p variants={textVariants} className={Styles.text} initial="initial" animate="animate"> <span className={Styles.span}> near you </span> </motion.p> */}
              <motion.div className={Styles.btn} variants={textVariants} initial="initial" animate="animate">
             <Link to="/workers"> <motion.button variants={textVariants} className={Styles.button}>Discover Talent</motion.button></Link>
             
@@ -72,8 +70,8 @@ const Hero = () => {
           </div>
           <motion.div className={Styles.right} >
           <div className={Styles.heroBackgrdR} ></div>
-            <motion.p className={Styles.pRight} variants={textVariantsRight} initial="initial" animate="animate"> Catch your next<br /> job <span style={{color:"white", fontWeight:"700"}}>  opportunity</span> </motion.p>
-            <motion.p variants={textVariantsRight} className={Styles.textRight} initial="initial" animate="animate"> <span> today.</span> </motion.p>
+            <motion.p className={Styles.pRight} variants={textVariantsRight} initial="initial" animate="animate"> Catch your next<br /> job <span style={{color:"white", fontWeight:"700"}}>  opportunity</span> today</motion.p>
+            {/* <motion.p variants={textVariantsRight} className={Styles.textRight} initial="initial" animate="animate"> today. </motion.p> */}
            <Link to="/jobs" style={{zIndex:"10"}}><motion.button variants={textVariantsRight} initial="initial" animate="animate" className={Styles.button2}>Browse Jobs</motion.button></Link> 
           </motion.div>
         </motion.div>
